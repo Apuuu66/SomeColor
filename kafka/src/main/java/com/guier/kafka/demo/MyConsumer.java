1aba055e03b907ca1bf553f294c5d31067cbd3a3
Guiee <466007639@qq.com>
2019-08-23 21:29:00 +0800

    exctption

---
+++ kafka/src/main/java/com/guier/kafka/demo/MyConsumer.java
@@ -0,0 +1,208 @@
+package com.guier.kafka.demo;
+
+
+import org.apache.kafka.clients.consumer.CommitFailedException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.util.Collections;
+import java.util.Properties;
+
+
+public class MyConsumer {
+    private static String topic="test-topic";
+    private static KafkaConsumer<String, String> consumer;
+    private static Properties properties;
+
+    static {
+
+        properties = new Properties();
+
+        properties.put("bootstrap.servers", "192.168.44.131:9092");
+        properties.put("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "KafkaStudy");
+        properties.put("max.poll.records", 100);
+        properties.put("max.poll.interval.ms", 600000);
+
+    }
+
+    private static void generalConsumeMessageAutoCommit() {
+
+        properties.put("enable.auto.commit", true);
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singleton(topic));
+
+        try {
+            while (true) {
+
+                boolean flag = true;
+                ConsumerRecords<String, String> records = consumer.poll(100);
+
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format(
+                            "topic = %s, partition = %s, key = %s, value = %s",
+                            record.topic(), record.partition(),
+                            record.key(), record.value()
+                    ));
+                    if (record.value().equals("done")) {
+                        flag = false;
+                    }
+                }
+
+                if (!flag) {
+                    break;
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+
+    private static void generalConsumeMessageSyncCommit() {
+
+        properties.put("auto.commit.offset", false);
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while (true) {
+            boolean flag = true;
+
+            ConsumerRecords<String, String> records =
+                    consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(String.format(
+                        "topic = %s, partition = %s, key = %s, value = %s",
+                        record.topic(), record.partition(),
+                        record.key(), record.value()
+                ));
+                if (record.value().equals("done")) {
+                    flag = false;
+                }
+            }
+
+            try {
+                consumer.commitSync();
+            } catch (CommitFailedException ex) {
+                System.out.println("commit failed error: "
+                        + ex.getMessage());
+            }
+
+            if (!flag) {
+                break;
+            }
+        }
+    }
+
+    private static void generalConsumeMessageAsyncCommit() {
+
+        properties.put("auto.commit.offset", false);
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while (true) {
+            boolean flag = true;
+
+            ConsumerRecords<String, String> records =
+                    consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(String.format(
+                        "topic = %s, partition = %s, key = %s, value = %s",
+                        record.topic(), record.partition(),
+                        record.key(), record.value()
+                ));
+                if (record.value().equals("done")) {
+                    flag = false;
+                }
+            }
+
+            // commit A, offset 2000
+            // commit B, offset 3000
+            consumer.commitAsync();
+
+            if (!flag) {
+                break;
+            }
+        }
+    }
+
+    private static void generalConsumeMessageAsyncCommitWithCallback() {
+
+        properties.put("auto.commit.offset", false);
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        while (true) {
+            boolean flag = true;
+
+            ConsumerRecords<String, String> records =
+                    consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(String.format(
+                        "topic = %s, partition = %s, key = %s, value = %s",
+                        record.topic(), record.partition(),
+                        record.key(), record.value()
+                ));
+                if (record.value().equals("done")) {
+                    flag = false;
+                }
+            }
+
+            consumer.commitAsync((map, e) -> {
+                if (e != null) {
+                    System.out.println("commit failed for offsets: " +
+                            e.getMessage());
+                }
+            });
+
+            if (!flag) {
+                break;
+            }
+        }
+    }
+
+    @SuppressWarnings("all")
+    private static void mixSyncAndAsyncCommit() {
+
+        properties.put("auto.commit.offset", false);
+        consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+
+        try {
+
+            while (true) {
+                ConsumerRecords<String, String> records =
+                        consumer.poll(100);
+
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println(String.format(
+                            "topic = %s, partition = %s, key = %s, " +
+                                    "value = %s",
+                            record.topic(), record.partition(),
+                            record.key(), record.value()
+                    ));
+                }
+
+                consumer.commitAsync();
+            }
+        } catch (Exception ex) {
+            System.out.println("commit async error: " + ex.getMessage());
+        } finally {
+            try {
+                consumer.commitSync();
+            } finally {
+                consumer.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Logger.getLogger("org").setLevel(Level.INFO);
+        generalConsumeMessageAutoCommit();
+    }
+}
